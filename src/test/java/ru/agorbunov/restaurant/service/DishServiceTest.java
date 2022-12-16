ee8fbffd94bb1fe6be80021a9e037bcab056bac6
Андрей <andrunov@gmail.com>
2022-12-16 21:56:39 +0300

    remove redundant parameter and refactoring

--- src/test/java/ru/agorbunov/restaurant/service/DishServiceTest.java
+++ src/test/java/ru/agorbunov/restaurant/service/DishServiceTest.java
@@ -8,9 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
-import ru.agorbunov.restaurant.UserTestData;
 import ru.agorbunov.restaurant.model.Dish;
-import ru.agorbunov.restaurant.util.exception.NoRightsException;
 import ru.agorbunov.restaurant.util.exception.NotFoundException;
 
 import static ru.agorbunov.restaurant.DishTestData.*;
@@ -24,73 +22,61 @@ import static ru.agorbunov.restaurant.DishTestData.*;
 public class DishServiceTest {
 
     @Autowired
-    private DishService dishService;
+    private DishService service;
 
     @Test
     public void save() throws Exception {
-        dishService.update(DISH_CREATED, UserTestData.USER_01_ID);
-        Assert.assertEquals(21, dishService.getAll().size());
+        service.update(DISH_CREATED);
+        Assert.assertEquals(21, service.getAll().size());
 
     }
 
 
     @Test(expected = IllegalArgumentException.class)
     public void saveNull() throws Exception {
-        dishService.update(null, UserTestData.USER_01_ID);
+        service.update(null);
     }
 
     @Test(expected = NotFoundException.class)
     public void delete() throws Exception {
-        dishService.delete(DISH_03_ID, UserTestData.USER_01_ID);
-        dishService.get(DISH_03_ID);
-    }
-
-    @Test(expected = NoRightsException.class)
-    public void deleteNoRights() throws Exception {
-        dishService.delete(DISH_03_ID, UserTestData.USER_00_ID);
+        service.delete(DISH_03_ID);
+        service.get(DISH_03_ID);
     }
 
 
     @Test(expected = NotFoundException.class)
     public void deleteNotFound() throws Exception {
-        dishService.delete(10, UserTestData.USER_01_ID);
+        service.delete(10);
     }
 
     @Test
     public void getAll() throws Exception {
-        Assert.assertEquals(20, dishService.getAll().size());
+        Assert.assertEquals(20, service.getAll().size());
     }
 
     @Test
     public void get() throws Exception {
-        Dish dish = dishService.get(DISH_01_ID);
+        Dish dish = service.get(DISH_01_ID);
         MATCHER.assertEquals(DISH_01, dish);
     }
 
 
     @Test(expected = NotFoundException.class)
     public void getNotFound() throws Exception {
-        dishService.get(10);
+        service.get(10);
     }
 
     @Test
     public void update() throws Exception{
-        Dish dish = dishService.get(DISH_02_ID);
+        Dish dish = service.get(DISH_02_ID);
         dish.setName("обновленное название");
-        dishService.update(dish, UserTestData.USER_01_ID);
+        service.update(dish);
         Assert.assertEquals(dish.getName(), "обновленное название");
-    }
-
-    @Test(expected = NoRightsException.class)
-    public void updateNoRights() throws Exception{
-        Dish dish = dishService.get(DISH_02_ID);
-        dish.setName("обновленное название");
-        dishService.update(dish, UserTestData.USER_00_ID);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void updateNull() throws Exception {
-        dishService.update(null, UserTestData.USER_01_ID);
+        service.update(null);
     }
 
 
